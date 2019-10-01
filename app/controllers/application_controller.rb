a27b7ccd9cf7ef26620e61905fc22bfdce58325b
Sanveg Rane <ssrane2@ncsu.edu>
2019-10-01 12:13:58 -0400

    Added hold flow and removed extra routes from requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!         # authenticate user before any controller action
 
+  # Prevent redirect if route doesn't exist
+  rescue_from ActionController::RoutingError do |exception|
+    flash[:error] = "There is no such route"
+    redirect_to root_url
+  end
 end
